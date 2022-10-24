b28be767c163fce18018a1ebddc040fa7bb72abd
EndureBlaze <hi@imyan.ren>
2022-10-24 18:41:02 +0800

    fix style

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
+import postCssPresetEnv from 'postcss-preset-env'
 
 export default defineConfig({
   plugins: [solidPlugin()],
   build: {
     target: 'esnext',
+  },
+  css: {
+    postcss: {
+      plugins: [postCssPresetEnv({ stage: 0 })],
+    },
   },
 })
